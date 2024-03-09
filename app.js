3c021ca6801f69c97f63e4b5d74f42dd537579f5
Pruthvi Bhat <111227965+PruthviGBhat@users.noreply.github.com>
2024-03-09 16:42:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
 import reservationRouter from "./routes/reservation.js";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
